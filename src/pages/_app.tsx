7d38b03ef98fa798f177620bbceeee7f09e60af7
Levan Beroshvili <v-levanbe@microsoft.com>
2023-05-09 20:03:44 +0400

    Added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import { LogsBufferContext } from 'contexts/LogsBufferContext';
 import '../styles/globals.css';
 import { LogsBuffer } from 'models/LogsBuffer';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <LogsBufferContext.Provider value={LogsBuffer}>
-      <Component {...pageProps} />
-    </LogsBufferContext.Provider>
+    <>
+      <LogsBufferContext.Provider value={LogsBuffer}>
+        <Component {...pageProps} />
+      </LogsBufferContext.Provider>
+      <Analytics />
+    </>
   );
 }
 
