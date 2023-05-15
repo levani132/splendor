cf5d3441ada4d8257488a44f6f6d6b27998344f4
Levan Beroshvili <v-levanbe@microsoft.com>
2023-05-15 12:40:05 +0400

    Fixed board background height and paddings

--- src/components/Board/BoardBackground.tsx
+++ src/components/Board/BoardBackground.tsx
@@ -15,15 +15,16 @@ export const BoardBackground: FC = () => {
 
   return (
     <div
-      className="absolute inset-0 flex justify-center items-center h-screen px-32"
+      className="absolute inset-0 flex justify-center items-center h-screen px-52 py-64"
       style={{ perspective: `${perspective}px` }}
     >
       <div
-        className="relative box-content rounded-[200px] py-8 px-16 border-[100px] border-black board w-full"
+        className="relative box-content rounded-[200px] py-8 px-16 border-[100px] border-black board w-full h-full"
         style={{
           transform: `scale(${scale}) rotateX(${rotation}deg)`,
           top,
           minWidth: width,
+          minHeight: height,
         }}
       >
         <div style={{ width, height }} />
