0cf31f1b9de6f357d89d3175cd7856a692cdbf9c
Levan Beroshvili <v-levanbe@microsoft.com>
2023-05-15 12:41:58 +0400

    Removed size markers

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,14 +6,12 @@ import { Board } from './Board/Board';
 import { Logs } from './Logs';
 import { Players } from './Players/Players';
 import { BoardBackground } from './Board/BoardBackground';
-import { useWindowSize } from 'utils/useWindowSize';
 
 interface GameProps {
   game: GameModel;
 }
 
 export const Game: FC<GameProps> = ({ game }) => {
-  const { width, height } = useWindowSize();
   return (
     <GameContext.Provider value={game}>
       <div className="w-screen h-screen fixed pb-4">
@@ -21,9 +19,6 @@ export const Game: FC<GameProps> = ({ game }) => {
         <Board />
         <Players />
         <Logs />
-        <div className="absolute inset-0 z-40 text-xl">
-          {width}x{height}
-        </div>
       </div>
     </GameContext.Provider>
   );
